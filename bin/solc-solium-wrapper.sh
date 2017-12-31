3e423de6edab1f194908270d0d9138dcf9c65597
Denis Glotov <denis@glotov.org>
2017-12-31 11:28:24 +0000

    Solidity prettified, solium linter support ❄️

--- bin/solc-solium-wrapper.sh
+++ bin/solc-solium-wrapper.sh
@@ -2,6 +2,8 @@
 #
 # Wrapper for running both solc and solium from flycheck-solidity.
 #
+
+#echo $1 >>/tmp/solc-solium-wrapper.log
+set -e
 solc $1
-cd $(dirname $1)
-solium -R gcc -f $(basename $1)
+cd $(dirname $1) && solium -R gcc -c ../.soliumrc.json -f $(basename $1)
--- lisp/init-solidity.el
+++ lisp/init-solidity.el
@@ -1,4 +1,12 @@
 ;; Explicitly require what needs to be.
 (require 'flycheck)
+
+(setq-default solidity-flycheck-solc-checker-active t)
+(setq-default solidity-flycheck-solium-checker-active nil)
+(setq-default solidity-solc-path "~/.emacs.d/bin/solc-solium-wrapper.sh")
 (require 'solidity-mode)
-(setq solidity-solc-path "~/.emacs.d/bin/solc-solium-wrapper.sh")
+
+(with-eval-after-load 'solidity-mode
+  (setq-default c-basic-offset 4))
+
+(provide 'init-solidity)

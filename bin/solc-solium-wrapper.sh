9e36054b818615786f0833e21205d4316cac2309
Denis Glotov <denis@glotov.org>
2018-07-15 12:00:07 +0300

    solc wrapper updated to handle openzeppelin-solidity

--- bin/solc-solium-wrapper.sh
+++ bin/solc-solium-wrapper.sh
@@ -3,7 +3,8 @@
 # Wrapper for running both solc and solium from flycheck-solidity.
 #
 
-#echo $1 >>/tmp/solc-solium-wrapper.log
+#echo $PWD: $@ >>/tmp/solc-solium-wrapper.log
 set -e
-solc $1 github.com=..
-cd $(dirname $1) && solium -R gcc -c .soliumrc.json -f $(basename $1)
+solc --allow-paths ../node_modules/zeppelin-solidity/ \
+     github.com=.. zeppelin-solidity=../node_modules/zeppelin-solidity $@
+#cd $(dirname $1) && solium -R gcc -c .soliumrc.json -f $(basename $1)

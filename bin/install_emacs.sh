ac850a5942f1ce9c6cd55bbbdbf3b20d1d1c5f8b
Denis Glotov <denis@glotov.org>
2019-09-22 20:45:51 +0300

    updates

--- bin/install_emacs.sh
+++ bin/install_emacs.sh
@@ -5,15 +5,18 @@ check() {
     command -v $1 >/dev/null
 }
 
-echo "Installing emacs..."
+echo "Installing emacs${EMACS_VERSION}..."
 sudo add-apt-repository -y ppa:kelleyk/emacs
 sudo apt-get install emacs${EMACS_VERSION}-nox
 sudo apt-get install ispell
 
-if ! grep -qe '~/.emacs.d/profile.sh' ~/.bashrc; then
+echo
+if grep -qe '~/.emacs.d/profile.sh' ~/.bashrc; then
+    echo "[Info] .bashrc already include our shell"
+else
     echo "Appending our shell to .bashrc..."
     echo "source ~/.emacs.d/profile.sh" >>~/.bashrc
 fi
 
 echo
-echo "All done: $(emacs --version)."
+echo "All done: $(emacs${EMACS_VERSION} --version)."

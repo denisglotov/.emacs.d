84610a5bcf715f86b000c76ea8c89b185856a0bf
Denis Glotov <denis@glotov.org>
2019-09-23 12:17:49 +0300

    Install latest emacs from ppa:kelleyk, travis

--- bin/install_emacs.sh
+++ bin/install_emacs.sh
@@ -1,15 +1,30 @@
 #!/bin/sh
+[ "$1" ] && EMACS_VERSION="$1" || EMACS_VERSION="26"
 
-if ! command -v emacs >/dev/null; then
-    echo "Installing emacs..."
-    sudo apt-get install emacs-nox
-fi
+check() {
+    command -v $1 >/dev/null
+}
 
-command -v ispell >/dev/null || sudo apt-get install ispell
+echo "Installing emacs${EMACS_VERSION}..."
+sudo add-apt-repository -y ppa:kelleyk/emacs
+sudo apt-get install emacs${EMACS_VERSION}-nox
+sudo apt-get install ispell
 
-if ! grep -qe '~/.emacs.d/profile.sh' ~/.bashrc; then
+echo
+if grep -qe '~/.emacs.d/profile.sh' ~/.bashrc; then
+    echo "[Info] .bashrc already include our shell"
+else
     echo "Appending our shell to .bashrc..."
     echo "source ~/.emacs.d/profile.sh" >>~/.bashrc
 fi
 
+echo
+if [ "${EMACS_VERSION}" ]; then
+    echo "Creating symbolic links..."
+    sudo ln -sf emacs${EMACS_VERSION} /usr/bin/emacs
+    sudo ln -sf emacsclient${EMACS_VERSION} /usr/bin/emacsclient
+fi
+
+echo
 echo "All done."
+emacs --version
